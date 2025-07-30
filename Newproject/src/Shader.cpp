016a2080ecb941dba0355bd045b14fe51b75c0e7
beki-github <bereketendale9@gmail.com>
2025-07-30 17:07:24 -0300

    fixed an error

--- Newproject/src/Shader.cpp
+++ Newproject/src/Shader.cpp
@@ -65,7 +65,7 @@ void Shader::setInt(const std::string& name, int value)const {
 }
 
 void Shader::setFloat(const std::string& name, float value)const {
-	glUniform1i(glGetUniformLocation(ID, name.c_str()),value);
+	glUniform1f(glGetUniformLocation(ID, name.c_str()),value);
 }
 
 void Shader::checkCompilationError(unsigned int shader, const std::string& type) {
