ce8b494f5729d443ca7c845111aa56e67aa98d35
beki-github <bereketendale9@gmail.com>
2025-08-28 14:39:21 -0300

    new repo

--- Newproject/src/utils_gl.cpp
+++ Newproject/src/utils_gl.cpp
@@ -26,4 +26,25 @@
         // Unbind VAO
         glBindVertexArray(0);
     }
+    void setBufferObjects2(GLuint& VAO, GLuint& VBO, GLuint& EBO, const float* vertices, size_t vertices_size, const unsigned int* indices, size_t indices_size) {
+        glGenVertexArrays(1, &VAO);
+        glBindVertexArray(VAO);
 
+        // VBO setup
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, vertices_size, vertices, GL_STATIC_DRAW);
+
+        // EBO setup
+        glGenBuffers(1, &EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices_size, indices, GL_STATIC_DRAW);
+
+        // Vertex attributes (position: 3 floats, color: 3 floats, UV: 2 floats)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+        // Unbind VAO
+        glBindVertexArray(0);
+    }
--- Newproject/src/utils_gl.h
+++ Newproject/src/utils_gl.h
@@ -5,4 +5,4 @@
 
 	// Set up VAO, VBO, EBO with vertex and index data
 	void setBufferObjects(GLuint& VAO, GLuint& VBO, GLuint& EBO, const float* vertices, size_t vertices_size, const unsigned int* indices, size_t indices_size);
-
+	void setBufferObjects2(GLuint& VAO, GLuint& VBO, GLuint& EBO, const float* vertices, size_t vertices_size, const unsigned int* indices, size_t indices_size);
